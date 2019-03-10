f5c2e2180952d3a21a3437485c4a87fe1b8fb020
Cxuyang <caoxuyang0317@qq.com>
2019-03-10 19:12:08 +0800

    update geo

--- components/public/header/geo.vue
+++ components/public/header/geo.vue
@@ -2,7 +2,7 @@
   <div class="m-geo">
     <i class="el-icon-location"/>
     {{ $store.state.geo.position.city }}
-    <nuxt-link class="changeCity" to="/changeCit">切换城市</nuxt-link>
+    <nuxt-link class="changeCity" to="/changeCity">切换城市</nuxt-link>
     {{ [citys.join(' ')] }}
   </div>
 </template>
--- server/interface/geo.js
+++ server/interface/geo.js
@@ -66,6 +66,34 @@ router.get('/province/:id', async (ctx) => {
   }
 })
 
+router.get('/city', async (ctx) => {
+  // let city = []
+  // let result = await City.find()
+  // result.forEach(item => {
+  //   city = city.concat(item.value)
+  // })
+  // ctx.body = {
+  //   code: 0,
+  //   city: city.map(item => {
+  //     return {
+  //       province: item.province,
+  //       id: item.id,
+  //       name: item.name === '市辖区' || item.name === '省直辖县级行政区划' ? item.province : item.name
+  //     }
+  //   })
+  // }
+  let {status, data: {city}} = await axios.get(`http://cp-tools.cn/geo/city?sign=${sign}`)
+  if (status === 200) {
+    ctx.body = {
+      city
+    }
+  } else {
+    ctx.body = {
+      city: []
+    }
+  }
+})
+
 router.get('hotCity', async (ctx) => {
   // let list = [ '北京市', '上海市', '广州市', '深圳市', '天津市', '西安市', '杭州市', '南京市', '武汉市', '成都市']
   // let result = await City.find()
